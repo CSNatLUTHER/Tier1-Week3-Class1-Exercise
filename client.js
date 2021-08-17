728734870a2bd1e99459d15cbab27b84bb3d6b9a
Chris Nelson <cnelson04@gmail.com>
2021-08-16 21:31:11 -0500

    added else if statement to loop in funciton

--- client.js
+++ client.js
@@ -5,9 +5,12 @@ function sentenceToSay( sentence ) {
     if(sentence[sentence.length -1] === '!'){
         return sentence += '!!';
     } //end if
-    return sentence += '!';
-    //return sentence = sentence + '!';
-}
-console.log (sentenceToSay('This is a test!'));
-alert (sentenceToSay('This is a test!'));
+    else if(sentence[sentence.length-1] === '.' || sentence[sentence.length-1] === '?'){
+        let slicedSentence = sentence.slice ( 0, -1 )
+        return slicedSentence += '!';
+    } // end else if
+    else {return sentence += '!';
+    } //end else   
+    } // end fucntion call
+console.log(sentenceToSay('This is a test!'));
 

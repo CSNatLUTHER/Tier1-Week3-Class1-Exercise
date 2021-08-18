2319870a8b6bf70144f9a1ca2f6672bb6f21b493
Chris Nelson <cnelson04@gmail.com>
2021-08-17 21:14:50 -0500

    tested indexOf of Characters and converting strings to numbers

--- client.js
+++ client.js
@@ -14,3 +14,14 @@ function sentenceToSay( sentence ) {
     } // end fucntion call
 console.log(sentenceToSay('This is a test!'));
 
+let numString = '267'
+
+let numDigit = Number(numString) // Yes, This Works to convert String to Number
+// let numDigit = numString * 1; // Yes, This works to convert String to Number
+
+
+console.log('numDigit:', numDigit);
+console.log( numDigit * 2 );
+console.log( numString );
+console.log( numString * 2 );
+
